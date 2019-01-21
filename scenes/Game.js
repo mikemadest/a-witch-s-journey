ff12e82583333e544db16e7498495c6d492537b7
Mike <mickael@mkms.fr>
2019-01-21 19:20:08 +0100

    Not much : - anims creation moved to Preload.create(); - experiments with collision.
    Still got a problem with tilemap layer collision.
    Got to try a few thing but before i want to get more features working.
    
    @todo : lot of code need cleanup, refactoring.
    I'll probably create an entity prefab and use it as a base for player and monster creation.
    First i want to have things working and be sure of how things are done.

--- scenes/Game.js
+++ scenes/Game.js
@@ -4,7 +4,7 @@
 
 class Game extends Phaser.Scene {
   constructor() {
-    super({key: "Game", active: false});
+    super({ key: "Game", active: false });
   }
 
   init() {
@@ -12,13 +12,15 @@ class Game extends Phaser.Scene {
   }
 
   create() {
-
     // main tile map
 
     this.map = this.add.tilemap("level1");
-    const backgroundTile = this.map.addTilesetImage('overworld', 'gameTile');
-    const objectsTile = this.map.addTilesetImage('objects', 'objectsTile');
-    const characterTile = this.map.addTilesetImage('character', 'charactersTile');
+    const backgroundTile = this.map.addTilesetImage("overworld", "gameTile");
+    const objectsTile = this.map.addTilesetImage("objects", "objectsTile");
+    const characterTile = this.map.addTilesetImage(
+      "character",
+      "charactersTile"
+    );
     this.backgroundLayer = this.map.createStaticLayer(
       "background",
       backgroundTile,
@@ -27,8 +29,18 @@ class Game extends Phaser.Scene {
     );
     //this.backgroundLayer.setCollisionByProperty({collides: true});
 
-    this.detailsLayer = this.map.createStaticLayer("details", backgroundTile, 0, 0);
-    this.housesLayer = this.map.createStaticLayer("houses", backgroundTile, 0, 0);
+    this.detailsLayer = this.map.createStaticLayer(
+      "details",
+      backgroundTile,
+      0,
+      0
+    );
+    this.housesLayer = this.map.createStaticLayer(
+      "houses",
+      backgroundTile,
+      0,
+      0
+    );
     //this.housesLayer.enableBody = true;
     //this.housesLayer.setCollisionByProperty({ collides: true });
     //this.housesLayer.setCollisionByExclusion([-1]);
@@ -51,25 +63,26 @@ class Game extends Phaser.Scene {
 
     // player @todo move to own class
 
-    this.player = this.physics.add.sprite(145, 587, "worldAnim", 'walkdown-1');
+    this.player = this.physics.add.sprite(145, 587, "worldAnim", "walkdown-1");
     this.player.enableBody();
     this.player.body.bounce.setTo(0.9, 0.9);
     this.player.setCollideWorldBounds(true);
     this.player.onWorldBounds = true;
-
 
     // coins
     this.coins = this.physics.add.group();
     this.coins.enableBody = true;
     var result = this.findObjectsByType("coin", this.map);
     result.forEach(function(element) {
-      const tmp = this.physics.add.sprite(element.x, element.y, "coinanim", 132);
+      const tmp = this.physics.add.sprite(
+        element.x,
+        element.y,
+        "coinanim",
+        132
+      );
       tmp.anims.play("spr-coin", true);
       this.coins.add(tmp);
     }, this);
-
-
-
 
     // collisions
 
@@ -81,28 +94,44 @@ class Game extends Phaser.Scene {
     //this.physics.add.collider(this.player, this.housesLayer);
     //this.physics.add.overlap(this.player, this.housesLayer, this.overHouse, null, this);
     //this.physics.add.collider(this.player, this.coins);
-    this.physics.add.overlap(this.player, this.coins, this.collectCoin, null, this);
+    this.physics.add.overlap(
+      this.player,
+      this.coins,
+      this.collectCoin,
+      null,
+      this
+    );
 
     //this.map.setCollisionBetween(1, 200);
     //this.housesLayer.resizeWorld();
     //this.housesLayer.debug = true;
 
-
-
     this.cursors = this.input.keyboard.createCursorKeys();
 
     // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
-    this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    this.cameras.main.setBounds(
+      0,
+      0,
+      this.map.widthInPixels,
+      this.map.heightInPixels
+    );
     //camera.startFollow(this.player);
     this.cameraDolly = new Phaser.Geom.Point(this.player.x, this.player.y);
     this.cameras.main.startFollow(this.cameraDolly);
 
-    this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    this.physics.world.setBounds(
+      0,
+      0,
+      this.map.widthInPixels,
+      this.map.heightInPixels
+    );
 
     this.playerScore = 0;
-    this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '16px', fill: '#fff' });
+    this.scoreText = this.add.text(16, 16, "Score: 0", {
+      fontSize: "16px",
+      fill: "#fff"
+    });
     this.scoreText.setScrollFactor(0);
-
 
     // debug
 
@@ -119,12 +148,6 @@ class Game extends Phaser.Scene {
 
     //
   }
-
-
-
-
-
-
 
   update(time, delta) {
     this.playerSpeed = 1.3;
@@ -147,63 +170,53 @@ class Game extends Phaser.Scene {
     this.cameraDolly.y = Math.floor(this.player.y);
   }
 
-
-
   overHouse() {
-    console.log('over a house !');
+    console.log("over a house !");
   }
 
-
-
-
-
-
+  /**
+   * collectCoin - action when user overlap a coin
+   *
+   * @param  {type} player description
+   * @param  {type} coin   description
+   * @return {type}        description
+   */
   collectCoin(player, coin) {
     coin.disableBody(true, true);
     this.playerScore++;
-    this.scoreText.setText('Score: ' + this.playerScore);
+    this.scoreText.setText("Score: " + this.playerScore);
     if (this.coins.countActive(true) === 0) {
       // toutes les pièces ont été trouvées : première quête terminée !!
     }
   }
 
-
-
-
-
+  /**
+   * findObjectsByType - helper to use object layer for element position
+   *
+   * @param  {type} type description
+   * @param  {type} map  description
+   * @return {type}      description
+   */
   findObjectsByType(type, map) {
     var result = [];
-
-    console.log('map = ', map);
-
-    if (map && map.objects && map.objects[0].objects) {
-      map.objects[0].objects.forEach(element => {
-        if (element.type === type) {
-          element.y -= map.tileHeight;
-          result.push(element);
-        }
-      });
+    if (!map || !map.objects || !map.objects[0].objects) {
       return result;
     }
+    map.objects[0].objects.forEach(element => {
+      if (element.type === type) {
+        element.y -= map.tileHeight;
+        result.push(element);
+      }
+    });
     return result;
   }
 
-
-
-
-
-
-
-
-
-
-
   render() {
-    const debugGraphics = this.add.graphics().setAlpha(0.75);
+    /*const debugGraphics = this.add.graphics().setAlpha(0.75);
     this.backgroundLayer.renderDebug(debugGraphics, {
       tileColor: null, // Color of non-colliding tiles
       collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
       faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-    });
+    });*/
   }
 }
